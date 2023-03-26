c58b7e06c2550f7f16a23c8aedad163e4ebfe536
RubedoLeimon <97351473+RubedoLeimon@users.noreply.github.com>
2023-03-26 16:40:46 -0500

    Further Fixes Copium

---
+++ Whitelist-Server-main/Server Set Up EXE/scripts/ezlibs-custom/custom.lua
@@ -0,0 +1,6 @@
+local CustPlugin = {}
+
+local helpers = require('scripts/ezlibs-scripts/helpers')
+local ezcheckpoints = helpers.safe_require('scripts/ezlibs-custom/ezcheckpoints')
+
+return CustPlugin
